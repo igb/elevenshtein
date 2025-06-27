c662f8be4ce671f5a529fcf603914f2402ddf892
Ian Brown <ibrown@netflix.com>
2025-06-26 20:43:08 -0700

    function interface

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,11 @@ fn elevenshtein(env: &Env) -> emacs::Result<String> {
     Ok("Hello from Rust!".to_string())
 }
 
+#[defun]
+fn edit_distance(env: &Env, source: String, target: String) -> emacs::Result<i64> {
+    Ok(0)
+}
+
 #[emacs::module(name = "elevenshtein")]
 fn init(_: &Env) -> emacs::Result<()> {
     // one-time module initialisation goes here if you need it
