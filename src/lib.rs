3e6daf47d6ae90533101736895e96caf306bde93
Ian Brown <ibrown@netflix.com>
2025-06-27 12:04:16 -0700

    setting up initial values

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,20 @@ fn elevenshtein(_env: &Env) -> emacs::Result<String> {
 fn edit_distance(_env: &Env, source: String, target: String) -> emacs::Result<i32> {
     let cols: usize = source.chars().count();
     let rows: usize = target.chars().count();
-    let mut matrix = create_matrix(cols, rows);
+    let mut _matrix = create_matrix(cols, rows);
     Ok(0)
 }
 
+// generate a 2D vector to store DP progress and final score
 fn create_matrix(cols: usize, rows: usize) -> Vec<Vec<i32>> {
-    return vec![vec![0; cols]; rows];
+    let mut matrix: Vec<Vec<i32>> = vec![vec![0; cols]; rows];
+    for n in 0..cols {
+        matrix[0][n] = n as i32;
+    }
+    for n in 1..rows {
+        matrix[n][0] = n as i32;
+    }
+    return matrix;
 }
 
 #[emacs::module(name = "elevenshtein")]
@@ -29,7 +37,7 @@ fn init(_: &Env) -> emacs::Result<()> {
 #[test]
 fn test_matrix_creation() {
     //Validate that the correct vector representation of a matrix is created.
-    assert_eq!(vec![vec![0, 0], vec![0, 0]], create_matrix(2, 2));
+    assert_eq!(vec![vec![0, 1], vec![1, 0]], create_matrix(2, 2));
     assert_eq!(vec![vec![0]], create_matrix(1, 1));
     assert_eq!(Vec::<Vec::<i32>>::new(), create_matrix(0, 0));
 }
