8db156f6b3f617af141478892afa2a7dedd04515
Ian Brown <ibrown@netflix.com>
2025-06-27 14:00:59 -0700

    initializing matrix

--- src/lib.rs
+++ src/lib.rs
@@ -17,11 +17,11 @@ fn edit_distance(_env: &Env, source: String, target: String) -> emacs::Result<i3
 
 // generate a 2D vector to store DP progress and final score
 fn create_matrix(cols: usize, rows: usize) -> Vec<Vec<i32>> {
-    let mut matrix: Vec<Vec<i32>> = vec![vec![0; cols]; rows];
-    for n in 0..cols {
+    let mut matrix: Vec<Vec<i32>> = vec![vec![0; cols + 1]; rows + 1];
+    for n in 1..cols + 1 {
         matrix[0][n] = n as i32;
     }
-    for n in 1..rows {
+    for n in 1..rows + 1 {
         matrix[n][0] = n as i32;
     }
     return matrix;
@@ -37,7 +37,21 @@ fn init(_: &Env) -> emacs::Result<()> {
 #[test]
 fn test_matrix_creation() {
     //Validate that the correct vector representation of a matrix is created.
-    assert_eq!(vec![vec![0, 1], vec![1, 0]], create_matrix(2, 2));
-    assert_eq!(vec![vec![0]], create_matrix(1, 1));
-    assert_eq!(Vec::<Vec::<i32>>::new(), create_matrix(0, 0));
+    assert_eq!(
+        vec![vec![0, 1, 2], vec![1, 0, 0], vec![2, 0, 0]],
+        create_matrix(2, 2)
+    );
+    assert_eq!(vec![vec![0, 1], vec![1, 0]], create_matrix(1, 1));
+    assert_eq!(vec![vec![0]], create_matrix(0, 0));
+    assert_eq!(
+        vec![
+            vec![0, 1, 2, 3, 4, 5],
+            vec![1, 0, 0, 0, 0, 0],
+            vec![2, 0, 0, 0, 0, 0],
+            vec![3, 0, 0, 0, 0, 0],
+            vec![4, 0, 0, 0, 0, 0],
+            vec![5, 0, 0, 0, 0, 0]
+        ],
+        create_matrix(5, 5)
+    );
 }
